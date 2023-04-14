f89485dcff9723241f7151758146c3f868dc6695
ahmedali288 <mohemmedahmed88@gmail.com>
2023-04-14 11:40:24 +0500

    dashboard added

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useContext } from 'react'
+import React, {useState, useContext} from "react";
 import { useNavigate } from "react-router-dom";
+import CanvasJSReact from "../lib/canvasjs.react";
 import { useBoardsData } from './features/customHooks';
 import AppContext from '../context/AppContext';
 
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 function Dashboard() {
   const Navigate = useNavigate();
@@ -17,25 +19,90 @@ function Dashboard() {
   }
 
   if (isBoardLoading) return 'Loading...';
-  // if (error) return `Error: ${error.message}`;
-  
-  return (
-    <div className="cards-list">
-      {boardsOverview.map((item, index)=>{
-        return(
-          <div key={index} onClick={()=>{Navigate(`/board/${item.id}`)}} className="card 1">
-            <div className="card_image"> 
-              <img src="https://i.redd.it/b3esnz5ra34y.jpg" /> 
-            </div>
-            <div className="card_title title-white">
-              <p>{item.name}</p>
-            </div>
-          </div>
-        )
-      })}
-    </div>
 
-  )
+  const charts = [];
+
+  for (let i = 0; i < boardsOverview.length; i++) {
+    const board = boardsOverview[i];
+    let totalTasks = 0;
+    let completedSubtasks = 0;
+
+    for (let j = 0; j < board.columns.length; j++) {
+      const column = board.columns[j];
+      totalTasks += column.tasks.length;
+
+      for (let k = 0; k < column.tasks.length; k++) {
+        const task = column.tasks[k];
+
+        for (let l = 0; l < task.subtasks.length; l++) {
+          const subtask = task.subtasks[l];
+
+          if (subtask.completed) {
+            completedSubtasks++;
+          }
+        }
+      }
+    }
+
+    if (completedSubtasks > 0) {
+      const dataPoints = [
+        { label: 'Completed', y: (completedSubtasks / totalTasks) * 100 },
+        { label: 'Not Completed', y: 100 - ((completedSubtasks / totalTasks) * 100) }
+      ];
+
+      const columnsList = board.columns.map((column) => {
+        return (
+          <li style={{listStyleType:"circle"}}>{column.name} ({column.tasks.length} tasks)</li>
+        );
+      });
+
+      const options = {
+        // title: {
+        //   text: `Board: ${board.name}`
+        // },
+        data: [
+          {
+            type: 'pie',
+            startAngle: 240,
+            yValueFormatString: '##0.00"%"',
+            indexLabel: '{label} {y}',
+            dataPoints: dataPoints
+          }
+        ]
+      };
+
+      const chart = (
+        <span>
+          <h2 onClick={()=>{Navigate(`/board/${board.id}`)}} className="canva-heading">{board.name}</h2>
+        <div style={{display:"flex"}}>
+          <CanvasJSChart options={options} />
+          <ul className="chart-ul">
+            <li style={{fontSize:"18px",fontWeight:"700"}}>Columns</li>
+            {columnsList}
+          </ul>
+        </div>
+        <div className="legend">
+          <div className="legend-item">
+            <div className="completed-color"></div>
+            <span>Completed</span>
+          </div>
+          <div className="legend-item">
+            <div className="not-completed-color"></div>
+            <span>Not Completed</span>
+          </div>
+        </div>
+        </span>
+      );
+
+      charts.push(chart);
+    }
+  }
+
+  return (
+    <div>
+      {charts}
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
